61c6ad0debecad9964f7a3ec76af3d5b1ca98c0f
Jules Raimbaud <jules.raimbaud@gmail.com>
2024-09-15 14:33:48 +0200

    add style to infomodal

--- components/InfoModal.js
+++ components/InfoModal.js
@@ -21,9 +21,184 @@ const InfoModal = ({ visible, onClose }) => {
 
           <ScrollView>
             <Text style={styles.modalTitle}>Informations sur le Jeu</Text>
+
+            <Text style={styles.modalSectionTitle}>Prix par grille :</Text>
             <Text style={styles.modalText}>
-              {/* Votre texte ici */}
+              <Text style={styles.highlight}>- Grille simple : </Text>2€
+              {'\n'}
+              <Text style={styles.highlight}>- Option Second Tirage : </Text>+1€ par grille
             </Text>
+
+            <Text style={styles.modalSectionTitle}>Gains possibles :</Text>
+
+            {/* Affichage des combinaisons avec des boules colorées */}
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 5 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+                <View style={[styles.ball, styles.redBall]}>
+                  <Text style={styles.ballText}>+</Text>
+                </View>
+              </View>
+              <Text style={styles.modalText}>= Jackpot</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 5 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 100 000€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 4 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+                <View style={[styles.ball, styles.redBall]}>
+                  <Text style={styles.ballText}>+</Text>
+                </View>
+              </View>
+              <Text style={styles.modalText}>= 1 000€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 4 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 400€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 3 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+                <View style={[styles.ball, styles.redBall]}>
+                  <Text style={styles.ballText}>+</Text>
+                </View>
+              </View>
+              <Text style={styles.modalText}>= 50€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 3 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 20€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 2 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+                <View style={[styles.ball, styles.redBall]}>
+                  <Text style={styles.ballText}>+</Text>
+                </View>
+              </View>
+              <Text style={styles.modalText}>= 10€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 2 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 4€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                <View style={[styles.ball, styles.blueBall]}>
+                  <Text style={styles.ballText}>#</Text>
+                </View>
+                <View style={[styles.ball, styles.redBall]}>
+                  <Text style={styles.ballText}>+</Text>
+                </View>
+              </View>
+              <Text style={styles.modalText}>= 2€ </Text>
+            </View>
+
+            {/* Nouveau cas pour un seul numéro chance */}
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                <View style={[styles.ball, styles.redBall]}>
+                  <Text style={styles.ballText}>+</Text>
+                </View>
+              </View>
+              <Text style={styles.modalText}>= 2€ </Text>
+            </View>
+
+            <Text style={styles.modalSectionTitle}>Gains Second Tirage :</Text>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 5 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 100 000€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 4 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 1 000€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 3 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 10€</Text>
+            </View>
+
+            <View style={styles.gainCategory}>
+              <View style={styles.numbersContainer}>
+                {Array.from({ length: 2 }).map((_, idx) => (
+                  <View key={idx} style={[styles.ball, styles.blueBall]}>
+                    <Text style={styles.ballText}>#</Text>
+                  </View>
+                ))}
+              </View>
+              <Text style={styles.modalText}>= 2€</Text>
+            </View>
           </ScrollView>
         </View>
       </View>
@@ -36,7 +211,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.7)',
     justifyContent: 'center',
-    alignItems: 'center', 
+    alignItems: 'center',
     padding: 10,
   },
   modalContent: {
@@ -45,17 +220,24 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     maxHeight: '80%',
     alignItems: 'center',
-    width: '90%', 
+    width: '90%',
   },
   modalTitle: {
-    fontSize: 16,
-    marginBottom: 8,
-    color: '#000000', 
+    fontSize: 20,
+    marginBottom: 10,
+    color: '#000000',
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  modalSectionTitle: {
+    fontSize: 18,
+    marginTop: 20,
+    marginBottom: 10,
+    color: '#000000',
+    fontWeight: 'bold',
+  },
   modalText: {
-    fontSize: 14,
+    fontSize: 16,
     color: '#333333',
     textAlign: 'left',
     marginVertical: 5,
@@ -67,6 +249,38 @@ const styles = StyleSheet.create({
   closeButtonText: {
     color: '#000',
     fontSize: 18,
+  },
+  gainCategory: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  numbersContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  ball: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 5,
+  },
+  ballText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  blueBall: {
+    backgroundColor: '#0055A4',
+  },
+  redBall: {
+    backgroundColor: '#E50000',
+  },
+  highlight: {
+    fontWeight: 'bold',
   },
 });
 
